ab54050b51818a883ad02aedb01b7a25e4698ed4
Soroush Fathi <s3000@yahoo.com>
2020-02-08 22:20:58 +0330

    Added UI

---
+++ public/precache-manifest.23e0875a8f44de18bf3512cd216518d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5c278b93f3475ba8c0314f40aadb22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bec69a703de984bf923d",
+    "url": "/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "3f37a6b161b74a6bd53b",
+    "url": "/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "bec69a703de984bf923d",
+    "url": "/static/js/2.9925d24a.chunk.js"
+  },
+  {
+    "revision": "1fca7bce4609f5bfdc2be99b893a58f6",
+    "url": "/static/js/2.9925d24a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f37a6b161b74a6bd53b",
+    "url": "/static/js/main.5d833632.chunk.js"
+  },
+  {
+    "revision": "f9c298ee9a66ed6a8433",
+    "url": "/static/js/runtime-main.68d29fbc.js"
+  }
+]);
